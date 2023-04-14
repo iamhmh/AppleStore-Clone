335779411e9fe7834c4dc60db1b80a30e480ff84
HMH <musicbyhmh@gmail.com>
2023-04-14 15:27:38 +0200

    maj

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+const navbarDropdownToggles = document.querySelectorAll(".navbar .dropdown-toggle");
+navbarDropdownToggles.forEach(function(toggle) {
+  toggle.addEventListener("mouseover", function() {
+    this.parentNode.classList.toggle("show");
+    this.parentNode.querySelector(".dropdown-menu").classList.toggle("show");
+  });
+});
+
+const navbarDropdownMenus = document.querySelectorAll(".navbar .dropdown-menu");
+navbarDropdownMenus.forEach(function(menu) {
+  menu.addEventListener("mouseleave", function() {
+    this.classList.remove("show");
+  });
+});
+
 document.addEventListener( 'DOMContentLoaded', function () {
   new Splide( '#image-carousel', 
     {
